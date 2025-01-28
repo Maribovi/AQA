599a1495588e57b44961dc57f341f4b48287b9b5
Maria Tretiakova <mailerbowler@yandex.ru>
2025-01-28 20:37:26 +0300

    Update HW1.java

--- src/main/java/org/example/HW1.java
+++ src/main/java/org/example/HW1.java
@@ -29,24 +29,36 @@ public class HW1 {
         int b = -1;
         int sum = a + b;
 
-        if (sum >= 0) System.out.println("Сумма положительная");
-        else System.out.println("Сумма отрицательная");
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 
     static void printColor() {
         int value = 5;
 
-        if (value <= 0) System.out.println("Красный");
-        if (value > 0 && value <= 100) System.out.println("Желтый");
-        if (value > 100) System.out.println("Зеленый");
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
     }
 
     static void compareNumbers() {
         int a = 1;
         int b = 2;
 
-        if (a >= b) System.out.println("a >= b");
-        else System.out.println("a < b");
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 
     static boolean isInRange(int a, int b) {
@@ -56,8 +68,11 @@ public class HW1 {
     }
 
     static void checkPositive(int x) {
-        if (x >= 0) System.out.println("Положительное");
-        else System.out.println("Отрицательное");
+        if (x >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
     }
 
     static boolean isNegative(int x) {
@@ -84,9 +99,11 @@ public class HW1 {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 1) {
                 array[i] = 0;
-            } else array[i] = 1;
+            } else {
+                array[i] = 1;
+            }
+            System.out.println(Arrays.toString(array));
         }
-        System.out.println(Arrays.toString(array));
     }
 
     static void fillInArray() {
@@ -102,7 +119,9 @@ public class HW1 {
     static void multiplyArrayUnderSix() {
         int[] arr = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 1};
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) arr[i] = arr[i] * 2;
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
         }
         System.out.println(Arrays.toString(arr));
     }
@@ -116,7 +135,9 @@ public class HW1 {
             for (j = 0; j < arr[i].length; j++, x--) {
                 if (i == j || i == x - 1) {
                     arr[i][j] = 1;
-                } else arr[i][j] = 0;
+                } else {
+                    arr[i][j] = 0;
+                }
                 System.out.print(" " + arr[i][j] + " ");
             }
             System.out.println();
@@ -126,7 +147,7 @@ public class HW1 {
     static int[] createArrayWithLength(int len, int initialValue) {
         int[] arr = new int[len];
 
-        for (int a:arr) {
+        for (int a : arr) {
             arr[a] = initialValue;
             System.out.print(arr[a] + " ");
         }
