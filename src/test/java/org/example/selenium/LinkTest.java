6f97b13701fbf3fbe5da4ad67e30831f44d3221e
Maria Tretiakova <mailerbowler@yandex.ru>
2025-03-12 16:56:08 +0300

    Update LinkTest.java

--- src/test/java/org/example/selenium/LinkTest.java
+++ src/test/java/org/example/selenium/LinkTest.java
@@ -12,6 +12,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.time.Duration;
 
 public class LinkTest {
